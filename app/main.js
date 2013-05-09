dd88942e1559c4f534c0eac0e5db0fd68aed0bde
Lee Bock <lbock@esri.com>
2013-05-09 16:40:23 -0400

    de-webmap-ify

--- app/main.js
+++ app/main.js
@@ -8,9 +8,8 @@ dojo.require("esri.map");
 *******************************************************/
 
 var TITLE = "This is the title."
-var BYLINE = "This is the byline"
-var WEBMAP_ID = "3732b8a6d0bc4a09b00247e8daf69af8";
-var GEOMETRY_SERVICE_URL = "http://tasks.arcgisonline.com/ArcGIS/rest/services/Geometry/GeometryServer";
+var BYLINE = "This is the byline";
+var BASEMAP_SERVICE_NATGEO = "http://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer";
 
 /******************************************************
 ***************** end config section ******************
@@ -64,30 +63,16 @@ function init() {
 	$("#title").append(TITLE);
 	$("#subtitle").append(BYLINE);	
 
-	var mapDeferred = esri.arcgis.utils.createMap(WEBMAP_ID, "map", {
-		mapOptions: {
-			slider: false,
-			wrapAround180: true,
-			extent:_homeExtent
-		},
-		ignorePopups: false,
-		geometryServiceURL: GEOMETRY_SERVICE_URL
-	});
-	
-	mapDeferred.addCallback(function(response) {	  
+	_map = new esri.Map("map");
+	_map.addLayer(new esri.layers.ArcGISTiledMapServiceLayer(BASEMAP_SERVICE_NATGEO));
 
-		_map = response.map;
-
-		if(_map.loaded){
+	if(_map.loaded){
+		initMap();
+	} else {
+		dojo.connect(_map,"onLoad",function(){
 			initMap();
-		} else {
-			dojo.connect(_map,"onLoad",function(){
-				initMap();
-			});
-		}
-				
-	});
-	
+		});
+	}
 }
 
 function initMap() {
